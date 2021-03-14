acf554ea3a7875f1e8fac021d1bc814ef7f411d3
nikhilmarathe2681 <71487169+nikhilmarathe2681@users.noreply.github.com>
2021-03-14 10:40:24 +0530

    Update header.php

--- header.php
+++ header.php
@@ -40,7 +40,8 @@
                     session_start();
                     session_unset();
                     session_destroy();
-                    header('location:index.php');
+                     // header('location:index.php');
+                    echo "<script>window.location.href='../index.php'</script>";
                 }
     
                 ?>
